a6efc6d50d4ab9164720b4cbdb38bda0863fa0f6
WShenxue <wanshenxue@163.com>
2019-07-10 21:28:12 +0800

    add week2 test

--- pythondemo/pythonprogram/week2/TurtleDemo.py
+++ pythondemo/pythonprogram/week2/TurtleDemo.py
@@ -1,8 +1,21 @@
 import turtle
+#绘制风车
+for i in range(4):
+    turtle.seth((i + 1)*90)
+    turtle.fd(150)
+    turtle.seth(i * 90)
+    turtle.circle(-150, 45)
+    turtle.goto(0, 0)
+turtle.done()
+
+
+
+
+
 # 绘制Z
-turtle.left(45)
-turtle.fd(150)
-turtle.right(135)
-turtle.fd(300)
-turtle.left(135)
-turtle.fd(150)
+# turtle.left(45)
+# turtle.fd(150)
+# turtle.right(135)
+# turtle.fd(300)
+# turtle.left(135)
+# turtle.fd(150)
---
+++ pythondemo/pythonprogram/week2/week2Test.py
@@ -0,0 +1,5 @@
+import turtle
+for i in range(8):
+    turtle.fd(150)
+    turtle.left(135)
+turtle.done()
